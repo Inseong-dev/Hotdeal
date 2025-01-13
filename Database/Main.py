99f2c78129ad562989e74226473fde26de5f33fd
Inseong-dev <73svnptre@gmail.com>
2025-01-13 14:13:50 +0000

    Rename: Change File name 파일 이름 변경

--- Database/Main.py
+++
@@ -1,7 +0,0 @@
-import CrawlPage
-import Mydata
-#import CrawllingPage # type: ignore
-#CollectionID는 Name으로 고정
-CollectionID = "Name"
-Page = CrawlPage.returnList()
-Mydata.add_Data(CollectionID, Page)
